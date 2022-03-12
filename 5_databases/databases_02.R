3d8341cd62a2adfd7111647def10a3c829f018ff
Kirill Müller <kirill@cynkra.com>
2022-03-12 07:42:12 +0100

    Finish second part

--- 5_databases/databases_02.R
+++ 5_databases/databases_02.R
@@ -13,12 +13,11 @@ slide_viewer <- function(path) {
 }
 # slide_viewer("5_databases/databases.html")
 
-### Reading tables from the database ###########################################
+### Computing on the database ##################################################
 
 # Connection -------------------------------------------------------------------
 
 con_duckdb <- DBI::dbConnect(duckdb::duckdb())
-copy_dm_to(con_duckdb, dm_pixarfilms(), set_key_constraints = FALSE, temporary = FALSE)
 
 # Lazy tables ------------------------------------------------------------------
 
@@ -39,16 +38,27 @@ pixar_films %>%
 pixar_films %>%
   collect()
 
-# Complex aggregation with transformation
+# Summaries
 pixar_films %>%
-  count(year(release_date))
+  group_by(film_rating) %>%
+  summarize(mean_run_time = mean(run_time)) %>%
+  ungroup()
+
+# Aggregation with transformation
+pixar_films %>%
+  mutate(release_year = year(release_date)) %>%
+  count(release_year)
 
 pixar_films %>%
-  add_count(year(release_date)) %>%
+  count(release_year = year(release_date))
+
+# Complex aggregation with transformation
+pixar_films %>%
+  add_count(release_year = year(release_date)) %>%
   filter(n > 1) %>%
   arrange(release_date)
 
-# Computations happens on the database (and you don't want to write that SQL!)
+# Computation happens on the database (and you don't want to write that SQL!)
 pixar_films %>%
   add_count(year(release_date)) %>%
   filter(n > 1) %>%
@@ -63,18 +73,147 @@ films_two_per_year <-
   collect()
 films_two_per_year
 
-# `slice()` --------------------------------------------------------------------
+# Caveat: order ----------------------------------------------------------------
+
+# Set-oriented, no intrinsic order, no `slice()`!
+
+# Bad:
+try(
+  pixar_films %>%
+    slice(1:3)
+)
+
+# Better:
+pixar_films_sqlite %>%
+  filter(between(row_number(), 1, 3))
+
+# Best, use `order_by` argument or `dbplyr::window_order()`
+pixar_films %>%
+  filter(between(row_number(release_date), 1, 3))
+
+pixar_films %>%
+  dbplyr::window_order(release_date) %>%
+  filter(between(row_number(), 1, 3))
+
+# Same for window functions:
+try(
+  pixar_films %>%
+    group_by(film_rating) %>%
+    mutate(total_run_time = cumsum(run_time)) %>%
+    ungroup()
+)
+
+pixar_films %>%
+  group_by(film_rating) %>%
+  dbplyr::window_order(release_date) %>%
+  mutate(total_run_time = cumsum(run_time)) %>%
+  ungroup()
+
+# Use `arrange()` to fix order, but only at the end of a query!
+
+# Bad:
+pixar_films %>%
+  arrange(film) %>%
+  mutate(run_time_hr = run_time / 60) %>%
+  filter(run_time_hr < 2)
+
+# Good:
+pixar_films %>%
+  mutate(run_time_hr = run_time / 60) %>%
+  filter(run_time_hr < 2) %>%
+  arrange(film)
+
+# NA sort first on databases!
+
+pixar_films %>%
+  arrange(run_time)
+
+pixar_films %>%
+  arrange(is.na(run_time), run_time)
+
+# Caveat: logical data type ----------------------------------------------------
+
+# Some databases have a logical data type...
+pixar_films %>%
+  count(film_rating == "PG")
+
+con_sqlite <- DBI::dbConnect(RSQLite::SQLite(), extended_types = TRUE)
+copy_dm_to(con_sqlite, dm_pixarfilms(), set_key_constraints = FALSE, temporary = FALSE)
+pixar_films_sqlite <- tbl(con_sqlite, "pixar_films")
+
+# Some don't (but coerce logical expressions to integers)...
+pixar_films_sqlite %>%
+  count(film_rating == "PG")
+
+# In SQL Server you have to work around:
+pixar_films %>%
+  count(if_else(film_rating == "PG", 1L, 0L))
+
+# Caveat: translations are not perfect -----------------------------------------
+
+# Integers versus numerics:
+pixar_films %>%
+  filter(run_time < 120) %>%
+  show_query()
+
+pixar_films %>%
+  filter(run_time < 120L) %>%
+  show_query()
+
+# Aggregation functions:
+pixar_films %>%
+  summarize(sum(run_time)) %>%
+  show_query()
+
+# Not all functions supported:
+try(
+  pixar_films %>%
+    filter(grepl("^Toy", film))
+)
+
+try(
+  pixar_films %>%
+    filter(str_detect(film, "^Toy"))
+)
+
+# Unknown functions are escaped:
+pixar_films %>%
+  filter(film %LIKE% "Toy%")
+
+try(
+  pixar_films %>%
+    collect() %>%
+    filter(film %LIKE% "Toy%")
+)
+
+pixar_films %>%
+  summarize(MAX(run_time))
+
+try(
+  pixar_films %>%
+    collect() %>%
+    summarize(MAX(run_time))
+)
+
+# SQL escaping:
+pixar_films %>%
+  mutate(number = CAST(number %AS% sql("integer")))
+
+pixar_films %>%
+  mutate(number = sql("CAST(number AS integer)"))
+
+pixar_films %>%
+  mutate(number = as.integer(number))
+
+# Exercises --------------------------------------------------------------------
 
 pixar_films
 
-# 1.  Return the 2 first rows using `slice()` then using `slice_head()`
-# 2.  Return the 2 last rows using `slice()` then using `slice_tail()`
-# 3.  Return the rows of the 2 shortest movies using `slice_min()`
-# 4.  Return the rows of the 15%  longest movies using `slice_max()` and the `prop` argument
-
-# `arrange()` ------------------------------------------------------------------
-
-pixar_films
-
-# 1.  Arrange `pixar_films` by name but starting with missing values (Hint: sort using `!is.na()` and break the ties with `film`)
-# 2.  Arrange `pixar_films` to have the shortest film with a "PG" rating on top (Hint: use `film_rating != "PG"`)
+# 1. Add new columns `release_year` and `release_month`.
+# 2. Use the new columns to compute the number of months since January 1970
+#    for each film
+# 3. Compute the overall median run time, and the median run time per film rating
+# 4. For each film except the last, compute how many days have passed until the next film.
+#     - Hint: Use `lag(..., order_by = ...)`
+# 4. Find the maximum number of days between releases of two G and two PG films.
+# 5. Experiment.
