4e73bb50e04f8e7f7ef67c22eb2a959817f4f78d
Kirill Müller <kirill@cynkra.com>
2022-03-12 17:44:36 +0100

    Micro ETL

---
+++ 5_databases/databases_04.R
@@ -0,0 +1,60 @@
+# attach relevant packages
+library(tidyverse)
+
+# display chosen presentation (it might take a few seconds to appear)
+slide_viewer <- function(path) {
+  tmp <- tempfile(fileext = ".html")
+  file.copy(path, tmp)
+  rstudioapi::viewer(tmp)
+}
+# slide_viewer("5_databases/databases.html")
+
+### Extract, Transform, Load ###################################################
+
+# Create target database -------------------------------------------------------
+
+db_path <- fs::path_abs("pixar.duckdb")
+db_path
+con <- DBI::dbConnect(duckdb::duckdb(dbdir = db_path))
+
+# Extract: Raw data ------------------------------------------------------------
+
+pixar_films_raw <- pixarfilms::pixar_films
+pixar_films_raw
+
+# Transform: Fix column type, extract sequel column ----------------------------
+
+pixar_films_clean <-
+  pixar_films_raw %>%
+  separate(film, into = c("franchise", "sequel"),
+    sep = " (?=[0-9]+$)", fill = "right", remove = FALSE
+  ) %>%
+  mutate(across(c(number, sequel), as.integer)) %>%
+  group_by(franchise) %>%
+  mutate(sequel = if_else(is.na(sequel) & n() > 1, 1L, sequel)) %>%
+  ungroup()
+pixar_films_clean
+
+# Load: Write table to the database --------------------------------------------
+
+DBI::dbWriteTable(con, "pixar_films", pixar_films_clean, overwrite = TRUE)
+DBI::dbExecute(con, "CREATE UNIQUE INDEX pixarfilms_pk ON pixar_films (film)")
+DBI::dbDisconnect(con)
+
+# Consume: share the file, open it ---------------------------------------------
+
+fs::dir_info() %>%
+  arrange(desc(birth_time)) %>%
+  head(1)
+
+con <- DBI::dbConnect(duckdb::duckdb(dbdir = db_path, read_only = TRUE))
+pixar_films <- tbl(con, "pixar_films")
+pixar_films
+
+# Exercises --------------------------------------------------------------------
+
+pixar_films
+
+# 1. Adapt the ETL workflow to convert the `run_time` column to a duration.
+#    - Hint: Use `mutate()` with `hms::hms(minutes = ...)` .
+# 2. Re-run the workflow.
