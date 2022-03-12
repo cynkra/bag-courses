61a3bc769081cdf93a0e177802989701e36034f4
Kirill Müller <kirill@cynkra.com>
2022-03-12 19:06:42 +0100

    Final setup

--- 5_databases/databases_22.R
+++ 5_databases/databases_22.R
@@ -1,6 +1,3 @@
-# install.packages(c("tidyverse", "dm", "DiagrammeR", "RSQLite", "duckdb", "progress", "pixarfilms", "nycflights13"), type = "binary")
-# pak::pak(c("tidyverse", "dm", "DiagrammeR", "RSQLite", "duckdb", "progress", "pixarfilms", "nycflights13"))
-
 # attach relevant packages
 library(tidyverse)
 library(dm)
@@ -50,70 +47,87 @@ dm_pixarfilms(consistent = TRUE) %>%
 
 # Joins ----
 
-# Bad:
-pixar_dm$academy %>%
-  left_join(pixar_dm$pixar_films %>% select(film, release_date))
-
-# Good:
-pixar_films_join <-
-  pixar_dm$pixar_films %>%
-  select(film, release_date)
-pixar_dm$academy %>%
-  left_join(pixar_films_join, by = "film")
-
-# With dm and zooming:
+# With zooming:
 pixar_dm %>%
   dm_zoom_to(academy) %>%
   left_join(pixar_films, select = c(film, release_date))
 
-# With dm and flattening:
+# With flattening:
 pixar_dm %>%
   dm_flatten_to_tbl(academy)
 
-# Copy to database ----
+# Composing locally ----
 
-con_sqlite <- DBI::dbConnect(RSQLite::SQLite(), extended_types = TRUE)
-pixar_dm_sqlite <- copy_dm_to(con_sqlite, dm_pixarfilms(consistent = TRUE), temporary = FALSE)
+pixar_films_clean <-
+  pixarfilms::pixar_films %>%
+  filter(!is.na(film)) %>%
+  separate(film, into = c("franchise", "sequel"),
+    sep = " (?=[0-9]+$)", fill = "right", remove = FALSE
+  ) %>%
+  mutate(across(c(number, sequel), as.integer)) %>%
+  group_by(franchise) %>%
+  mutate(sequel = if_else(is.na(sequel) & n() > 1, 1L, sequel)) %>%
+  ungroup()
 
-pixar_dm_sqlite
-pixar_dm_sqlite %>%
+base_dm <- dm(
+  pixar_films = pixar_films_clean,
+  academy = pixarfilms::academy,
+  box_office = pixarfilms::box_office,
+)
+base_dm
+
+base_dm %>%
+  dm_draw()
+
+full_dm <-
+  base_dm %>%
+  dm_add_pk(pixar_films, film) %>%
+  dm_add_pk(box_office, film) %>%
+  dm_add_fk(academy, film, pixar_films) %>%
+  dm_add_fk(box_office, film, pixar_films)
+
+full_dm %>%
+  dm_draw(view_type = "all")
+
+# ETL, revisited ----
+
+db_path <- fs::path_abs("pixar.duckdb")
+fs::file_delete(db_path)
+con <- DBI::dbConnect(duckdb::duckdb(dbdir = db_path))
+pixar_dm_duckdb <- copy_dm_to(con, full_dm, temporary = FALSE)
+
+pixar_dm_duckdb
+pixar_dm_duckdb %>%
   dm_draw()
 
 # Permanent tables, opt-in:
-pixar_dm_sqlite %>%
+pixar_dm_duckdb %>%
   dm_get_tables()
 
 # All dm operations work the same way for databases and for local sources:
-pixar_dm_sqlite %>%
+pixar_dm_duckdb %>%
   dm_flatten_to_tbl(academy)
+
+DBI::dbDisconnect(con)
 
 # Learning from database ----
 
-pixar_dm_learned <- dm_from_src(con_sqlite)
+con <- DBI::dbConnect(duckdb::duckdb(dbdir = db_path))
+pixar_dm_learned <- dm_from_src(con)
 
 pixar_dm_learned %>%
   dm_draw()
 
 pixar_dm_learned %>%
   dm_add_pk(pixar_films, film) %>%
-  dm_add_pk(public_response, film) %>%
-  dm_add_pk(box_office, film) %>%
-  dm_draw()
-
-pixar_dm_learned %>%
-  dm_add_pk(pixar_films, film) %>%
-  dm_add_pk(public_response, film) %>%
-  dm_add_pk(box_office, film) %>%
   dm_add_fk(academy, film, pixar_films) %>%
-  dm_add_fk(box_office, film, pixar_films) %>%
-  dm_add_fk(genres, film, pixar_films) %>%
-  dm_add_fk(pixar_people, film, pixar_films) %>%
-  dm_add_fk(public_response, film, pixar_films) %>%
   dm_draw()
 
 dm_pixarfilms_small <- function() {
+  db_path <- fs::path_abs("pixar.duckdb")
+  con <- DBI::dbConnect(duckdb::duckdb(dbdir = db_path))
   table_names <- c("academy", "box_office", "pixar_films")
-  dm_from_src(con_sqlite, table_names = table_names, learn_keys = FALSE) %>%
+  dm_from_src(con, table_names = table_names, learn_keys = FALSE) %>%
     dm_add_pk(pixar_films, film) %>%
     dm_add_pk(box_office, film) %>%
     dm_add_fk(academy, film, pixar_films) %>%
@@ -122,3 +136,6 @@ dm_pixarfilms_small <- function() {
 
 dm_pixarfilms_small() %>%
   dm_draw()
+
+
+# FIXME: Split, exercises
