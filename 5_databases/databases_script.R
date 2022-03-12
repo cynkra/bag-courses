e11b3065a07810c04dcb735562e71dc075f847e1
Kirill Müller <kirill@cynkra.com>
2022-03-12 06:13:46 +0100

    Downsizing

--- 5_databases/databases_script.R
+++ 5_databases/databases_script.R
@@ -41,7 +41,7 @@ pixar_films
 pixar_films %>%
   collect()
 
-# Derived data -----------------------------------------------------------------
+# Downsizing on the database ---------------------------------------------------
 
 # Projection (column selection)
 pixar_films %>%
@@ -72,11 +72,24 @@ pixar_films %>%
   filter(release_date >= "2020-01-01") %>%
   show_query()
 
+# Bring the data into the R session
+df_pixar_films_202x <-
+  pixar_films %>%
+  filter(release_date >= "2020-01-01") %>%
+  collect()
+df_pixar_films_202x
+
 # Immutable data: original data unchanged
 pixar_films %>%
   collect()
 
 # Aggregation
+pixar_films %>%
+  group_by(film_rating) %>%
+  summarize(n = n()) %>%
+  ungroup()
+
+# Aggregation (shortcut)
 pixar_films %>%
   count(film_rating)
 
@@ -85,9 +98,54 @@ pixar_films %>%
   count(film_rating) %>%
   show_query()
 
+# Bring the data into the R session
+df_pixar_films_by_rating <-
+  pixar_films %>%
+  count(film_rating) %>%
+  collect()
+df_pixar_films_by_rating
+
 # Immutable data: original data unchanged
 pixar_films %>%
   collect()
+
+# Downsizing on the database: Exercises ----------------------------------------
+
+# `select()` -------------------------------------------------------------------
+
+pixar_films
+
+# *  Find several ways to select the 3 first columns
+# *  What happens if you include the name of a variable multiple times in a `select()` call?
+# *  Select all columns that contain underscores (use `contains()`)
+# *  Use `all_of()` to select 2 columns of your choice
+
+# `filter()` -------------------------------------------------------------------
+
+pixar_films
+
+# Find all films that
+# 1. Are rated "PG"
+# 2. Had a run time below 95
+# 3. Had a rating of "N/A" or "Not Rated"
+# 4. Were released after and including year 2020
+# 5. Have a missing name (`film` column) or `run_time`
+# 6. Were released after and including year 2020 with name not missing
+# 7. Are a first sequel (the name ends with "2")
+#     - Hint: Bring the data into the R session
+
+# `count()`, `group_by()`, `summarize()`, `ungroup()` --------------------------
+
+pixar_films
+
+# 1. How many films are stored in the table?
+# 2. How many films released after 2005 are stored in the table?
+# 3. What is the total run time of all films?
+# 4. What is the total run time of all films, per rating?
+
+
+
+
 
 # Transformation
 pixar_films %>%
@@ -125,29 +183,6 @@ films_two_per_year <-
   arrange(release_date) %>%
   collect()
 films_two_per_year
-
-# `select()` -------------------------------------------------------------------
-
-pixar_films
-
-# *  Find several ways to select the 3 first columns
-# *  What happens if you include the name of a variable multiple times in a `select()` call?
-# *  Select all columns that contain underscores (use `contains()`)
-# *  Use `all_of()` to select 2 columns of your choice
-
-# `filter()` -------------------------------------------------------------------
-
-pixar_films
-
-# Find all films that
-# 1. Are rated "PG"
-# 2. Had a run time below 95
-# 3. Had a rating of "N/A" or "Not Rated"
-# 4. Were released after and including year 2020
-# 5. Have a missing name (`film` column) or `run_time`
-# 6. Were released after and including year 2020 with name not missing
-# 7. Are a first sequel (the name ends with "2")
-#     - Hint: Bring the data into the R session
 
 # `slice()` --------------------------------------------------------------------
 
