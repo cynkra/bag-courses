2a9e30f3f9e8bb87a4e28d64453d0a1b0ef9a8c4
Kirill Müller <kirill@cynkra.com>
2022-03-13 06:39:49 +0100

    Import 1.1 + tweak

--- 5_databases/databases_11.R
+++ 5_databases/databases_11.R
@@ -3,6 +3,7 @@
 
 # attach relevant packages
 library(tidyverse)
+library(DBI)
 
 # display chosen presentation (it might take a few seconds to appear)
 slide_viewer <- function(path) {
@@ -16,137 +17,48 @@ slide_viewer <- function(path) {
 
 # Connection -------------------------------------------------------------------
 
-con_duckdb <- DBI::dbConnect(duckdb::duckdb())
-dm::copy_dm_to(con_duckdb, dm::dm_pixarfilms(), set_key_constraints = FALSE, temporary = FALSE)
+con_duckdb <- dbConnect(duckdb::duckdb())
+con_duckdb
+
+# Magic: import tables into the database
+dm::copy_dm_to(
+  con_duckdb,
+  dm::dm_pixarfilms(),
+  set_key_constraints = FALSE,
+  temporary = FALSE
+)
+
+# Discover tables --------------------------------------------------------------
+
+dbListTables(con_duckdb)
+dbListFields(con_duckdb, "pixar_films")
 
 # Read table -------------------------------------------------------------------
 
-DBI::dbListTables(con_duckdb)
-DBI::dbListFields(con_duckdb, "pixar_films")
-df_pixar_films <- DBI::dbReadTable(con_duckdb, "pixar_films")
+df_pixar_films <- dbReadTable(con_duckdb, "pixar_films")
 df_pixar_films
 as_tibble(df_pixar_films)
 
 # Execute queries --------------------------------------------------------------
 
-DBI::dbGetQuery(con_duckdb, "SELECT * FROM pixar_films")
-DBI::dbGetQuery(con_duckdb, "SELECT * FROM pixar_films WHERE release_date >= '2010-01-01'")
-DBI::dbGetQuery(con_duckdb, r"(SELECT * FROM "pixar_films" WHERE "release_date" >= '2020-01-01')")
+dbGetQuery(con_duckdb, "SELECT * FROM pixar_films")
 
-# Lazy tables ------------------------------------------------------------------
+sql <- "SELECT * FROM pixar_films WHERE release_date >= '2020-01-01'"
+# sql <- r"(SELECT * FROM "pixar_films" WHERE "release_date" >= '2020-01-01')"
+dbGetQuery(con_duckdb, sql)
 
-pixar_films <- tbl(con_duckdb, "pixar_films")
-pixar_films
+# Further pointers -------------------------------------------------------------
 
-df_pixar_films <-
-  pixar_films %>%
-  collect()
-df_pixar_films
+# Quoting identifiers
+dbQuoteIdentifier(con_duckdb, "academy")
 
-# View the first 1000 rows:
-pixar_films %>%
-  view()
+# Quoting literals
+dbQuoteLiteral(con_duckdb, "Toy Story")
+dbQuoteLiteral(con_duckdb, as.Date("2020-01-01"))
 
-# Downsizing on the database ---------------------------------------------------
+# Parameterized queries
+sql <- "SELECT count(*) FROM pixar_films WHERE release_date >= ?"
+dbGetQuery(con_duckdb, sql, params = list(as.Date("2020-01-01")))
 
-# Projection (column selection)
-pixar_films %>%
-  select(1:3)
+# Reading tables: Exercises ----------------------------------------------------
 
-# Computations happens on the database!
-pixar_films %>%
-  select(1:3) %>%
-  show_query()
-
-# Bring the data into the R session
-df_pixar_films_3 <-
-  pixar_films %>%
-  select(1:3) %>%
-  collect()
-df_pixar_films_3
-
-# Immutable data: original data unchanged
-pixar_films %>%
-  collect()
-
-# Subsetting
-pixar_films %>%
-  filter(release_date >= "2020-01-01")
-
-# Computations happens on the database!
-pixar_films %>%
-  filter(release_date >= "2020-01-01") %>%
-  show_query()
-
-# Bring the data into the R session
-df_pixar_films_202x <-
-  pixar_films %>%
-  filter(release_date >= "2020-01-01") %>%
-  collect()
-df_pixar_films_202x
-
-# Immutable data: original data unchanged
-pixar_films %>%
-  collect()
-
-# Aggregation
-pixar_films %>%
-  group_by(film_rating) %>%
-  summarize(n = n()) %>%
-  ungroup()
-
-# Aggregation (shortcut)
-pixar_films %>%
-  count(film_rating)
-
-# Computations happens on the database!
-pixar_films %>%
-  count(film_rating) %>%
-  show_query()
-
-# Bring the data into the R session
-df_pixar_films_by_rating <-
-  pixar_films %>%
-  count(film_rating) %>%
-  collect()
-df_pixar_films_by_rating
-
-# Immutable data: original data unchanged
-pixar_films %>%
-  collect()
-
-# Downsizing on the database: Exercises ----------------------------------------
-
-# `select()` -------------------------------------------------------------------
-
-pixar_films
-
-# *  Find several ways to select the 3 first columns
-# *  What happens if you include the name of a variable multiple times in a `select()` call?
-# *  Select all columns that contain underscores (use `contains()`)
-# *  Use `all_of()` to select 2 columns of your choice
-
-# `filter()` -------------------------------------------------------------------
-
-pixar_films
-
-# Find all films that
-# 1. Are rated "PG"
-# 2. Had a run time below 95
-# 3. Had a rating of "N/A" or "Not Rated"
-# 4. Were released after and including year 2020
-# 5. Have a missing name (`film` column) or `run_time`
-# 6. Were released after and including year 2020 with name not missing
-# 7. Are a first sequel (the name ends with "2")
-#     - Hint: Bring the data into the R session before filtering
-
-# `count()`, `summarize()`, `group_by()`, `ungroup()` --------------------------
-
-pixar_films
-
-# 1. How many films are stored in the table?
-# 2. How many films released after 2005 are stored in the table?
-# 3. What is the total run time of all films?
-#     - Hint: Use `summarize(sum(...))`, watch out for the warning
-# 4. What is the total run time of all films, per rating?
-#     - Hint: Use `group_by()`
